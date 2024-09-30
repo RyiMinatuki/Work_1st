f5480735707405a9829470e7a7cef304594b2477
RyiMinatuki <kogacyama@gmail.com>
2024-09-30 23:00:10 +0900

    20240930

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,21 +6,21 @@
 
 @section('content')
     <div class="attendance-container">
-        <h1>さんお疲れ様です！</h1>
+        <h1>{{ Auth::user()->name }}さんお疲れ様です！</h1>
         <div class="attendance-grid">
-            <form action="" method="POST">
+            <form action="{{ route('attendance.start') }}" method="POST">
                 @csrf
                 <button type="submit" class="attendance-button">勤務開始</button>
             </form>
-            <form action="" method="POST">
+            <form action="{{ route('attendance.end') }}" method="POST">
                 @csrf
                 <button type="submit" class="attendance-button">勤務終了</button>
             </form>
-            <form action="" method="POST">
+            <form action="{{ route('break.start') }}" method="POST">
                 @csrf
                 <button type="submit" class="attendance-button">休憩開始</button>
             </form>
-            <form action="" method="POST">
+            <form action="{{ route('break.end') }}" method="POST">
                 @csrf
                 <button type="submit" class="attendance-button">休憩終了</button>
             </form>
